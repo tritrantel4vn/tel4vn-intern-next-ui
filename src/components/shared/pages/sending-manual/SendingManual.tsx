25b792be49fbaaed49ba3f8422371204e92de119
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2025-07-16 17:33:44 +0700

    implement-sending_manual_page

--- src/components/shared/pages/sending-manual/SendingManual.tsx
+++ src/components/shared/pages/sending-manual/SendingManual.tsx
@@ -8,8 +8,9 @@ import { toast } from "react-toastify"
 import { ChangeEvent, useEffect, useState } from "react";
 import { BrandQueryParams } from "@type/api/brand.type";
 import { apiGetBrands } from "@api/brand";
-import { Template, TemplateQueryParams } from "@type/api/template.type";
 import { apiGetTemplate } from "@api/template";
+import RecieverInforTable from "@components/shared/non-shared/RecieverInforTable";
+import { SMSReview } from "@components/shared/atoms";
 
 const SendingManualPage = () => {
     const SENDING_MANUAL_BREADCRUMBS: BreadcrumbItem[] = [
@@ -31,9 +32,17 @@ const SendingManualPage = () => {
         value: "SMS",
         noTranslate: true,
     }]
-    const [brand_id, setBrandIDs] = useState<string>("");
-    const [templates, setTemplates] = useState<{ label: string; value: string }[]>([]);
+    const [brandId, setBrandIDs] = useState<string>("");
+    const [templates, setTemplates] = useState<{ label: string; value: string; content: string }[]>([]);
     const [selectedTemplate, setSelectedTemplate] = useState<string>("");
+    const [templateContent, setTemplateContent] = useState<string>("");
+    const [templateVariables, setTemplateVariables] = useState<string[]>([]);
+    const [sendList, setSendList] = useState<Array<Record<string, string>>>([
+        { phone_number: "" }
+    ]);
+    const selectedBrand = brands.find(b => b.value === brandId);
+    const selectedBrandName = selectedBrand?.label || "";
+
 
     useEffect(() => {
         const fetchBrands = async () => {
@@ -66,7 +75,7 @@ const SendingManualPage = () => {
         try {
             const response = await apiGetTemplate({ ...params })
             if (response.code === "OK") {
-                const filtered = response.data.filter((temp) => (temp.brand_id === brand_id))
+                const filtered = response.data.filter((temp) => (temp.brand_id === brandId))
                 const options = filtered.map((temp) => ({
                     noTranslate: true,
                     value: temp.id,
@@ -84,60 +93,105 @@ const SendingManualPage = () => {
         }
     };
     useEffect(() => {
-        if (brand_id) {
+        if (brandId) {
             fetchTemplate();
         }
-    }, [brand_id]);
+    }, [brandId]);
 
-    const handleOnclickReset = (()=>{
+    const handleOnclickReset = (() => {
         setBrandIDs("");
         setSelectedTemplate("");
     })
+    const updateSendList = (index: number, key: string, value: string) => {
+        const updated = [...sendList];
+        updated[index][key] = value;
+        setSendList(updated);
+    };
+
+    const isSendListValid = () => {
+        return sendList.every(row =>
+            Object.values(row).every(value => value.trim() !== "")
+        );
+    };
+
 
     return (
         <DefaultPageLayout breadcrumbs={SENDING_MANUAL_BREADCRUMBS}>
-            <div className=" flex w-8/12 flex-col gap-4">
+            <form className="flex items-start gap-4">
+                <div className=" flex w-8/12 flex-col gap-4">
+                    <SelectForm
+                        disabled
+                        options={channel}
+                        label={"sending-manual_page.channel"}
+                    />
 
-                <SelectForm
-                    disabled
-                    options={channel}
-                    label={"sending-manual_page.channel"}
-                />
+                    <SelectForm
+                        options={brands}
+                        label={"sending-manual_page.brand"}
+                        value={brandId}
+                        placeholder={t("select.placeholder", { data: t("sending-manual_page.brand").toLowerCase() })}
+                        onChange={(event: ChangeEvent<HTMLSelectElement>) => setBrandIDs(event.target.value)}
+                    />
 
-                <SelectForm
-                    options={brands}
-                    label={"sending-manual_page.brand"}
-                    value={brand_id}
-                    placeholder={t("select.placeholder", { data: t("sending-manual_page.brand").toLowerCase() })}
-                    onChange={(event: ChangeEvent<HTMLSelectElement>) => setBrandIDs(event.target.value)}
-                />
+                    <SelectForm
+                        options={templates}
+                        label={"sending-manual_page.template"}
+                        value={selectedTemplate}
+                        placeholder={t("select.placeholder", { data: t("sending-manual_page.template").toLowerCase() })}
+                        errorMessage={(brandId.length > 0 && selectedTemplate.length === 0) ? t("error_message.required") : ""}
+                        onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                            const selectedId = event.target.value;
+                            setSelectedTemplate(selectedId);
 
-                <SelectForm
-                    options={templates}
-                    label={"sending-manual_page.template"}
-                    value={selectedTemplate}
-                    placeholder={t("select.placeholder", { data: t("sending-manual_page.template").toLowerCase() })}
-                    errorMessage={(brand_id.length > 0 && selectedTemplate.length===0 ) ? t("error_message.required") : ""}
-                    onChange={(event: ChangeEvent<HTMLSelectElement>) => setSelectedTemplate(event.target.value) }
-                />
-                <div className="mt-10 flex justify-end gap-2">
-                    <Button
-                        variant="danger"
-                        outline
-                        type="reset"
-                        onClick={handleOnclickReset}>
-                        {t("sending-manual_page.reset")}
-                    </Button>
+                            const selected = templates.find(t => t.value === selectedId);
+                            const content = selected?.content || "";
+                            setTemplateContent(content);
 
-                    <Button
-                        variant="surface"
-                        type="submit"
-                    >
-                        {t("sending-manual_page.send")}
-                    </Button>
+                            // Extract variables from template
+                            const variables = content.match(/{{(.*?)}}/g)?.map(v => v.replace(/{{|}}/g, "")) || [];
+                            setTemplateVariables(variables);
+
+                            // Init sendList with 1 row
+                            const newRow: Record<string, string> = { phone: "" };
+                            variables.forEach((v) => newRow[v] = "");
+                            setSendList([newRow]);
+                        }}
+
+                    />
+                    <RecieverInforTable
+                        required
+                        templateVariables={templateVariables}
+                        sendList={sendList}
+                        updateSendList={updateSendList}
+                        setSendList={setSendList}
+                    />
+
+                    <div className="mt-10 flex justify-end gap-2">
+                        <Button
+                            variant="danger"
+                            outline
+                            type="reset"
+                            onClick={handleOnclickReset}>
+                            {t("sending-manual_page.reset")}
+                        </Button>
+
+                        <Button
+                            type="submit"
+                            disabled={!isSendListValid()}
+                        >
+                            {t("sending-manual_page.send")}
+                        </Button>
+                    </div>
                 </div>
-            </div>
-            
+                <div className="w-4/12">
+                    <SMSReview
+                        channel="SMS"
+                        brandName={selectedBrandName}
+                        messageContent={templateContent}
+                        phoneNumber={sendList[0]?.phone} />
+                </div>
+            </form>
+
         </DefaultPageLayout>
     )
 };
